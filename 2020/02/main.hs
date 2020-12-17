ea25d32eb5865df41c424f5c37b05ff4d1a59be1
harderj <harderjacob@gmail.com>
2020-12-17 22:01:22 +0100

    smal com

--- 2020/02/main.hs
+++ 2020/02/main.hs
@@ -1,2 +1,9 @@
 
+import Network.HTTP
+
+main = do
+  response <- simpleHTTP $ getRequest "https://adventofcode.com/2020/day/2/input"
+  let body = fmap rspBody response
+  print response
+  print body
 
