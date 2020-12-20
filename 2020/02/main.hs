3d5c44b8775108e9e633e79215ae2ced5fec0eb1
harderj <harderjacob@gmail.com>
2020-12-20 15:01:10 +0100

    day 2 complete

--- 2020/02/main.hs
+++ 2020/02/main.hs
@@ -1,9 +1,37 @@
 
-import Network.HTTP
+import Foreign.Marshal.Utils
+import Data.List.Split
 
+readInput = readFile "input.dat"
+
+-- list2Tuple lst = read $ "(" ++ (init.tail.show) lst ++ ")"
+
+main :: Num x => IO x
 main = do
-  response <- simpleHTTP $ getRequest "https://adventofcode.com/2020/day/2/input"
-  let body = fmap rspBody response
-  print response
-  print body
+  str <- readFile "input.dat"
+  return $ processInput str
+
+-- processInput :: Num x => String -> x
+processInput input = let
+  lines_ = map words $ lines input 
+  take2 :: [String] -> (Int, Int) 
+  take2 (a:b:_) = (read a, read b)
+  take2 a = error $ "Input error in range: " ++ show a
+  fromWords (w1:w2:w3:_) =
+    (take2 $ splitOn ['-'] w1
+    ,head w2
+    ,w3)
+  validate ((a, b), c, w) =
+    between a b $ countElem c w
+  in sum $ map (fromBool . validate . fromWords) lines_
+
+-- countElem :: (Eq e, Num x) => e -> [e] -> x
+countElem y (x:xs) = fromBool (x == y) + countElem y xs
+countElem _ [] = 0
+
+-- between :: Ord a => a -> a -> a -> Bool
+between a b c = (a <= c && c <= b)
+
+
+
 
