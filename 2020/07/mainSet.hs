81cea1702f858b8f2470b43be9cd450174696ed2
harderj <harderjacob@gmail.com>
2021-01-08 21:57:52 +0100

    day 7 pt. 2 done

--- 2020/07/mainSet.hs
+++ 2020/07/mainSet.hs
@@ -1,25 +1,24 @@
 
-import System.IO
-import Data.List
 import Data.Function
+import qualified Data.Set as Set
 
 data Edge k a = E k a
   deriving (Show, Eq, Ord)
-data Node k a = N k [Edge k a]
+data Node k a = N k (Set.Set (Edge k a))
   deriving (Show, Eq, Ord)
-type Graph k a = [Node k a]
+type Graph k a = Set.Set (Node k a)
 
 getKey :: Node k a -> k
 getKey (N k _) = k
 
 type Bag = Node String Int
-type Specification = [Bag]
+type Specification = Set.Set Bag
 
 loadSpecification :: String -> Specification
 loadSpecification cts = let
   lns :: [String]
   lns = lines cts
-  in map readBag lns
+  in Set.fromList $ map readBag lns
 
 readBag :: String -> Bag
 readBag ln = let
@@ -28,17 +27,17 @@ readBag ln = let
   h :: [String] -> [Edge String Int]
   h (n2:a2:c2:_:wds2) = E (unwords [a2, c2]) (read n2) : h wds2
   h _ = []
-  in N key (h wds1)
+  in N key (Set.fromList $ h wds1)
 
 isChildOf :: (Eq k) => k -> Node k a -> Bool
 isChildOf k (N _ es) = any (\(E k' _) -> k == k') es
 
 parents :: (Eq k) => Graph k a -> k -> Graph k a
-parents g k = filter (isChildOf k) g
+parents g k = Set.filter (isChildOf k) g
 
 oldies :: (Ord k, Ord a) => Graph k a -> Graph k a -> Graph k a
-oldies g ps = let qs = g \\ ps
-  in ps ++ (concat $ map (\(N k' _) -> parents qs k') ps)
+oldies g ps = let qs = g Set.\\ ps
+  in Set.union ps $ Set.unions $ Set.map (\(N k' _) -> parents qs k') ps
 
 ancestors :: (Ord k, Ord a) => Graph k a -> k -> Graph k a
 ancestors g k = fixEq (oldies g) $ parents g k
@@ -48,17 +47,17 @@ fixEq f x = let y = f x in
   if x == y then x else fixEq f y
 
 getNode :: (Ord k) => Graph k a -> k -> Node k a
-getNode g k = head $ filter (\(N k' _) -> k == k') g
+getNode g k = Set.findMin $ Set.filter (\(N k' _) -> k == k') g
 
-children :: (Ord k, Ord a) => Graph k a -> k -> [Node k a]
+children :: (Ord k, Ord a) => Graph k a -> k -> Set.Set (Node k a)
 children g k = let
   N _ es = getNode g k
-  in map (\(E k' _) -> getNode g k') es
+  in Set.map (\(E k' _) -> getNode g k') es
 
 accumulateEdges :: (Ord k, Ord a, Num a) => Graph k a -> k -> a
 accumulateEdges g k = (h $ getNode g k)
   where 
-    h (N k' es) = sum $ map h' es
+    h (N k' es) = sum $ map h' $ Set.toList es
     h' (E k' x) = x * (1 + accumulateEdges g k')
 
 main :: IO ()
@@ -68,32 +67,13 @@ mainRutine :: String -> IO ()
 mainRutine filename = do
   cts <- readFile filename
   putStrLn $ "part 1: " ++ (show $ part1 cts)
-  let s = loadSpecification cts
-      n = getNode s "shiny gold"
-      cs = children s "shiny gold"
-  -- mapM_ print $ s
-  -- print $ n
-  -- print $ Set.take 5 cs
   putStrLn $ "part 2: " ++ (show $ part2 cts)
 
 part1 :: String -> Int
 part1 cts = let s = loadSpecification cts
-  in length $ ancestors s "shiny gold"
+  in Set.size $ ancestors s "shiny gold"
 
 part2 :: String -> Int
 part2 cts = let s = loadSpecification cts
   in accumulateEdges s "shiny gold"
 
-test = do
-  cts <- readFile "input.dat"
-  let s = loadSpecification cts
-  handle <- openFile "compare.txt" WriteMode
-  mapM_ (h handle) s
-    
-h handle_ (N k es) = do
-  let n = length es
-      m = length $ nub es
-      b = n == m
-  hPutStrLn handle_ $ show (b, k)
-
-    
